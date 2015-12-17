823d286f9e4b2172cdebf014c1e6a3500f80c23b
DerekLoveCC <zhaomingbo1010@126.com>
2015-12-17 20:23:41 +0800

    add disposable pattern

---
+++ Design Pattern/IDisposePattern/BaseDisposableClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace IDisposePattern
+{
+    public class BaseDisposableClass : IDisposable
+    {
+        private bool _disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    DisposeManagedResource();
+                }
+
+                DisposeUnmanagedResource();
+
+                _disposed = true;
+            }
+        }
+
+        protected virtual void DisposeManagedResource()
+        {
+        }
+
+        protected virtual void DisposeUnmanagedResource()
+        {
+        }
+
+        ~BaseDisposableClass()
+        {
+            Dispose(false);
+        }
+    }
+}
