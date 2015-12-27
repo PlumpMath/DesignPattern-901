9afd98d062ccdc7b090b2a1668e7a5cfb691a98e
DerekLoveCC <zhaomingbo1010@126.com>
2015-12-27 13:16:29 +0800

    add composite pattern code

---
+++ Design Pattern/Composite/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Composite
+{
+    /// <summary>
+    /// 1. "lowest common denominator"
+    /// </summary>
+    internal interface Component
+    {
+        void Traverse();
+    }
+
+    /// <summary>
+    /// 2. "Is a" relationship
+    /// </summary>
+    internal class Primitive : Component
+    {
+        private int value;
+
+        public Primitive(int val)
+        {
+            value = val;
+        }
+
+        public void Traverse()
+        {
+            Console.WriteLine(value + "  ");
+        }
+    }
+
+    /// <summary>
+    /// 2. "has a" relationship
+    /// </summary>
+    internal abstract class Composite : Component
+    {
+        private Component[] children = new Component[9];  // 3. Couple to interface Component
+        private int total = 0;
+        private int value;
+
+        internal Composite(int val)
+        {
+            value = val;
+        }
+
+        public void Add(Component c)// 3. Couple to interface
+        {
+            children[total++] = c;
+        }
+
+        public virtual void Traverse()
+        {
+            Console.WriteLine(value + "  ");
+            for (var i = 0; i < total; i++)
+            {
+                children[i].Traverse(); // 4. Delegation and polymorphism
+            }
+        }
+    }
+
+    /// <summary>
+    ///  Two different kinds of "con-tainer" classes.  Most of the "meat" is in the Composite base class.
+    /// </summary>
+    internal class Row : Composite
+    {
+        public Row(int val) : base(val)
+        {
+        }
+
+        public override void Traverse()
+        {
+            Console.WriteLine("Row");
+            base.Traverse();
+        }
+
+        private class Column : Composite
+        {
+            public Column(int val) : base(val)
+            {
+            }
+
+            public override void Traverse()
+            {
+                Console.WriteLine("Col");
+                base.Traverse();
+            }
+        }
+
+        public class CompositeDemo
+        {
+            public static void Main(String[] args)
+            {
+                Composite first = new Row(1);          // Row1
+                Composite second = new Column(2);       //   |
+                Composite third = new Column(3);       //   +-- Col2
+                Composite fourth = new Row(4);          //   |     |
+                Composite fifth = new Row(5);          //   |     +-- 7
+                first.Add(second);                      //   +-- Col3
+                first.Add(third);                      //   |     |
+                third.Add(fourth);                      //   |     +-- Row4
+                third.Add(fifth);                      //   |     |     |
+                first.Add(new Primitive(6));         //   |     |     +-- 9
+                second.Add(new Primitive(7));         //   |     +-- Row5
+                third.Add(new Primitive(8));         //   |     |     |
+                fourth.Add(new Primitive(9));         //   |     |     +-- 10
+                fifth.Add(new Primitive(10));         //   |     +-- 8
+                first.Traverse();                         //   +-- 6
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Design Pattern/Composite/readme.md
@@ -0,0 +1,5 @@
+﻿
+1. Create a "lowest common denominator" that makes classes interchangeable
+2. All concrete classes declare an "isa" relationship to the interface
+3. All "container" classes couple themselves to the interface
+4. "Container" classes use polymorphism as they delegate to their children
