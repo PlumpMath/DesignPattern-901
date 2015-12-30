faedfdd3956b5778231175a59cf17a3d02eb1626
DerekLoveCC <zhaomingbo1010@126.com>
2015-12-30 22:13:11 +0800

    add private class data pattern

---
+++ Design Pattern/Circle/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+
+namespace Circle
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public class CircleData
+    {
+        private double _radius;
+        private Color _color;
+        private Point _origin;
+
+        public CircleData(double radius, Color color, Point origin)
+        {
+            this._radius = radius;
+            this._color = color;
+            this._origin = origin;
+        }
+
+        public double Radius
+        {
+            get { return this._radius; }
+        }
+
+        public Color Color
+        {
+            get { return this._color; }
+        }
+
+        public Point Origin
+        {
+            get { return this._origin; }
+        }
+    }
+
+    public class Circle
+    {
+        private CircleData circleData;
+
+        public Circle(double radius, Color color, Point origin)
+        {
+            this.circleData = new CircleData(radius, color, origin);
+        }
+
+        public double Circumference
+        {
+            get { return this.circleData.Radius * Math.PI; }
+        }
+
+        public double Diameter
+        {
+            get { return this.circleData.Radius * 2; }
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            //...
+        }
+    }
+}
