d4b4b24f55c2e88db44135f8f183f392fc7ef923
DerekLoveCC <zhaomingbo1010@126.com>
2016-01-03 22:39:43 +0800

    add interpreter pattern

---
+++ Design Pattern/Interpreter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var context = new Context("Hello World");
+            var root = new NonTerminalExpression
+            {
+                Expression1 = new TerminalExpression(),
+                Expression2 = new TerminalExpression()
+            };
+            root.Interpret(context);
+            Console.ReadKey();
+        }
+    }
+
+    public class Context
+    {
+        public Context(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    public abstract class ExpressionBase
+    {
+        public abstract void Interpret(Context context);
+    }
+
+    public class TerminalExpression : ExpressionBase
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Terminal Symbol {0}.", context.Name);
+        }
+    }
+
+    public class NonTerminalExpression : ExpressionBase
+    {
+        public ExpressionBase Expression1 { get; set; }
+        public ExpressionBase Expression2 { get; set; }
+
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("Non Terminal Symbol {0}.", context.Name);
+            Expression1.Interpret(context);
+            Expression2.Interpret(context);
+        }
+    }
+}
---
+++ Design Pattern/PolishNotation/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PolishNotation
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            IParser parser = new Parser();
+
+            var commands =
+              new[] { "+ 1 2", "- 3 4", "+ - 5 6 7", "+ 8 - 9 1", "+ - + - - 2 3 4 + - -5 6 + -7 8 9 0" };
+
+            foreach (var command in commands)
+            {
+                var expression = parser.Parse(command);
+                Console.WriteLine("{0} = {1}", expression, expression.Evaluate());
+            }
+
+            // Results:
+            // (1 + 2) = 3
+            // (3 - 4) = -1
+            // ((5 - 6) + 7) = 6
+            // (8 + (9 - 1)) = 16
+            // (((((2 - 3) - 4) + ((-5 - 6) + (-7 + 8))) - 9) + 0) = -24
+
+            Console.ReadKey();
+        }
+    }
+
+    public interface IExpression
+    {
+        int Evaluate();
+    }
+
+    public class IntegerTerminalExpression : IExpression
+    {
+        private readonly int _value;
+
+        public IntegerTerminalExpression(int value)
+        {
+            _value = value;
+        }
+
+        public int Evaluate()
+        {
+            return _value;
+        }
+
+        public override string ToString()
+        {
+            return _value.ToString();
+        }
+    }
+
+    public class AdditionNonterminalExpression : IExpression
+    {
+        private readonly IExpression _expr1;
+        private readonly IExpression _expr2;
+
+        public AdditionNonterminalExpression(
+          IExpression expr1,
+          IExpression expr2)
+        {
+            _expr1 = expr1;
+            _expr2 = expr2;
+        }
+
+        public int Evaluate()
+        {
+            var value1 = _expr1.Evaluate();
+            var value2 = _expr2.Evaluate();
+            return value1 + value2;
+        }
+
+        public override string ToString()
+        {
+            return $"({_expr1} + {_expr2})";
+        }
+    }
+
+    public class SubtractionNonterminalExpression : IExpression
+    {
+        private readonly IExpression _expr1;
+        private readonly IExpression _expr2;
+
+        public SubtractionNonterminalExpression(
+          IExpression expr1,
+          IExpression expr2)
+        {
+            _expr1 = expr1;
+            _expr2 = expr2;
+        }
+
+        public int Evaluate()
+        {
+            var value1 = _expr1.Evaluate();
+            var value2 = _expr2.Evaluate();
+            return value1 - value2;
+        }
+
+        public override string ToString()
+        {
+            return $"({_expr1} - {_expr2})";
+        }
+    }
+
+    public interface IParser
+    {
+        IExpression Parse(string polish);
+    }
+
+    public class Parser : IParser
+    {
+        public IExpression Parse(string polish)
+        {
+            var symbols = new List<string>(polish.Split(' '));
+            return ParseNextExpression(symbols);
+        }
+
+        private IExpression ParseNextExpression(List<string> symbols)
+        {
+            int value;
+            if (int.TryParse(symbols[0], out value))
+            {
+                symbols.RemoveAt(0);
+                return new IntegerTerminalExpression(value);
+            }
+            return ParseNonTerminalExpression(symbols);
+        }
+
+        private IExpression ParseNonTerminalExpression(List<string> symbols)
+        {
+            var symbol = symbols[0];
+            symbols.RemoveAt(0);
+
+            var expr1 = ParseNextExpression(symbols);
+            var expr2 = ParseNextExpression(symbols);
+
+            switch (symbol)
+            {
+                case "+":
+                    return new AdditionNonterminalExpression(expr1, expr2);
+
+                case "-":
+                    return new SubtractionNonterminalExpression(expr1, expr2);
+
+                default:
+                    {
+                        var message = $"Invalid Symbol ({symbol})";
+                        throw new InvalidOperationException(message);
+                    }
+            }
+        }
+    }
+}
